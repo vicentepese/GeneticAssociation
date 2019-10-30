bba1f28ea96463a9b91154cf80cfbb2ca3ad9dc3
Vicente Peris Sempere <vipese@sh-ln01.stanford.edu>
2019-10-29 18:34:17 -0700

    Check reproducibility first version

---
+++ checkRepro.py
@@ -0,0 +1,89 @@
+import json
+import os
+import csv
+import pandas as pd
+import numpy as np
+import gzip
+import sys
+from collections import OrderedDict
+import sys
+
+
+def loadReproData(filename):
+
+    # Create xl file
+    xlFile = pd.ExcelFile(filename)
+
+    # Create Ordered dictionnary
+    pre_repro_data = {
+        sheet_name: xlFile.parse(sheet_name)
+        for sheet_name in xlFile.sheet_names
+    }
+
+    pre_repro_data = pre_repro_data['MHC long range associations']
+    repro_data = OrderedDict()
+    for val in pre_repro_data.values:
+        repro_data[val[0]] = val[1:]
+
+    return repro_data
+
+def GzipFileHandler(FileName, Read=True):
+    if '.gz' in FileName:
+        if Read is True:
+            OpenFile = gzip.open(FileName, 'rb')
+
+        else:
+            OpenFile = gzip.open(FileName, 'wb')
+    else:
+        if Read is True:
+            OpenFile = open(FileName, 'r')
+
+        else:
+            OpenFile = open(FileName, 'w')
+
+    return OpenFile
+
+def loadCHRData(filename):
+
+    # Load file
+    OpenFile = GzipFileHandler(filename, Read=True)
+    CHR_data = pd.read_csv(filename, compression = 'gzip')
+
+    return CHR_data
+
+
+def getMatches(CHR_data, repro_data):
+
+    # Get genes
+    snps_pos = CHR_data['snps']
+
+    # Check if snps is among the significant results
+    for snp in snps_pos:
+        if snp in list(repro_data.keys()):
+
+            # Get index 
+            idx = list(repro_data.keys()).index(snp)
+
+            # Check if 
+
+
+def main():
+
+    # Load options
+    with open("optionsCheck.json", 'r') as jsonFile:
+        options = json.load(jsonFile)
+
+    # Load "reproducibility data"
+    repro_data = loadReproData(options['file']["checkRepro"])
+
+    # Load association analysis data
+    CHR_data = loadCHRData('CHR_14.csv.gz')
+
+    # Get matches
+    getMatches(CHR_data, repro_data)
+
+    # Load chromosome data
+    print('stop')
+
+
+main()
